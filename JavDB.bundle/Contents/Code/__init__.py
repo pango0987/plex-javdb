40d4264522a4de2ef1e625e671d15e7b71bc2359
pango0987 <49338744+pango0987@users.noreply.github.com>
2019-04-06 20:02:53 +0900

    Update __init__.py

--- JavDB.bundle/Contents/Code/__init__.py
+++ JavDB.bundle/Contents/Code/__init__.py
@@ -1,8 +1,7 @@
 SEARCH_URL = 'https://www.javbus.com/'
 
 def Start():
-    #HTTP.ClearCache()
-    HTTP.CacheTime = CACHE_1WEEK
+    HTTP.ClearCache()
     HTTP.Headers['User-agent'] = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)'
     HTTP.Headers['Accept-Encoding'] = 'utf-8'
 
